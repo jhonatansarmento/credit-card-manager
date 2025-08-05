a5e3e9842c3f0f95b6901ec3e27c9e9195a4045e
Jhonatan Sarmento <jhonatan.sarmento@fit-tecnologia.org.br>
2025-08-05 01:49:39 -0400

    feat: update build process to include Prisma generation and add Vercel configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
 import { Toaster } from '@/components/ui/sonner';
 import { ClerkProvider } from '@clerk/nextjs';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import type React from 'react';
 import './globals.css';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Controle de Dívidas',
